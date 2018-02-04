929ba1c7aa420d9a4d6db55fa50d2dcd2c7d7d52
Administrator <Administrator@749S7K6CZFKAHCH>
2018-02-05 07:22:56 +0800

    commit

---
+++ MyTestSc/src/com/topic/passPmpBook/PassPmpBook08_2.java
@@ -0,0 +1,25 @@
+package com.topic.passPmpBook;
+
+import com.base.BaseTest;
+
+/**
+ * 成功通过PMP
+ * 
+ * 第8章 测试题2 P185
+ * 
+ * @author JianDe
+ */
+public class PassPmpBook08_2 extends BaseTest {
+    public PassPmpBook08_2(String result) {
+        super("DDBDBACCDCADBABADDAD");
+    }
+
+    public static void main(String[] args) {
+        BaseTest pm1 = new PassPmpBook08_2(null);
+        pm1.test("DDBDB ACCDC ADBAB ADDAD"); // 2018-02-05 06:30 全对啦！666
+    }
+
+}
+/*
+ * 
+ */
