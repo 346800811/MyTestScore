c9de8ec12e33fb4cff603ab689a041d266604e82
Administrator <Administrator@749S7K6CZFKAHCH>
2018-02-04 21:08:24 +0800

    edit

---
+++ MyTestSc/src/com/topic/passPmpBook/PassPmpBook07_2.java
@@ -0,0 +1,23 @@
+package com.topic.passPmpBook;
+
+import com.base.BaseTest;
+
+/**
+ * 成功通过PMP
+ * 
+ * @author JianDe
+ */
+public class PassPmpBook07_2 extends BaseTest {
+	public PassPmpBook07_2(String result) {
+		super("");
+	}
+
+	public static void main(String[] args) {
+		BaseTest pm1 = new PassPmpBook07_2(null);
+		pm1.test(""); // 2018-02-02
+	}
+
+}
+/*
+ * 
+ */
