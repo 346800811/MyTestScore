a3b5ada8e39b846bf9a31350bd211a139c710654
De <346800811@qq.com>
2018-02-07 09:29:28 +0800

    commit

--- MyTestSc/src/com/base/BaseTest.java
+++ MyTestSc/src/com/base/BaseTest.java
@@ -1,39 +1,39 @@
 package com.base;
 
 public abstract class BaseTest {
-	/* 正确答案（连续大写的ABCD） */
-	private char[] result;
+    /* 正确答案（连续大写的ABCD） */
+    private char[] result;
 
-	/** 使用正确答案构造对象 */
-	public BaseTest(String result) {
-		this.result = result.toCharArray();
-	}
+    /** 使用正确答案构造对象 */
+    public BaseTest(String result) {
+        this.result = result.toCharArray();
+    }
 
-	/**
-	 * 阅，打分
-	 * 
-	 * @param me
-	 *            我的答案
-	 */
-	public void test(String me) {
-		ScoreCard sc = new ScoreCard();
-		StringBuilder sb = new StringBuilder();
-		int len = me.length();
-		for (int i = 0; i < len; i++) {
-			if (me.charAt(i) == ' ') {
-				sb.append(' ').append(sc.getCouTeam()).append('|');
-			} else {
-				if (result[sc.getCouTeam()] == me.charAt(i)) {
-					sc.addCouTue();
-					sb.append('.');
-				} else {
-					sc.addCouFal();
-					sb.append(me.charAt(i));
-				}
-			}
-		}
-		System.out.println(sb);
-		sc.printResult();
-	}
+    /**
+     * 阅，打分
+     * 
+     * @param me
+     *            我的答案
+     */
+    public void test(String me) {
+        ScoreCard sc = new ScoreCard();
+        StringBuilder sb = new StringBuilder();
+        int len = me.length();
+        for (int i = 0; i < len; i++) {
+            if (me.charAt(i) == ' ') {
+                sb.append(' ').append(sc.getCouTeam()).append('|');
+            } else {
+                if (result[sc.getCouTeam()] == me.charAt(i)) {
+                    sc.addCouTue();
+                    sb.append('.');
+                } else {
+                    sc.addCouFal();
+                    sb.append(me.charAt(i));
+                }
+            }
+        }
+        System.out.println(sb);
+        sc.printResult();
+    }
 
 }
--- MyTestSc/src/com/base/ScoreCard.java
+++ MyTestSc/src/com/base/ScoreCard.java
@@ -1,62 +1,62 @@
 package com.base;
 
 public class ScoreCard {
-	private int couTue = 0; // 正确数
-	private int couFal = 0; // 错误数
-	private int couTeam = 0; // 总题目数
-	private double score; // 分数（=正确数/总题目数）
+    private int couTue = 0; // 正确数
+    private int couFal = 0; // 错误数
+    private int couTeam = 0; // 总题目数
+    private double score; // 分数（=正确数/总题目数）
 
-	/** 获得做对的答案数 */
-	public int getCouTue() {
-		return couTue;
-	}
+    /** 获得做对的答案数 */
+    public int getCouTue() {
+        return couTue;
+    }
 
-	/** 获得做错的答案数 */
-	public int getCouFal() {
-		return couFal;
-	}
+    /** 获得做错的答案数 */
+    public int getCouFal() {
+        return couFal;
+    }
 
-	/** 获得做完的总题目数 */
-	public int getCouTeam() {
-		return couTeam;
-	}
+    /** 获得做完的总题目数 */
+    public int getCouTeam() {
+        return couTeam;
+    }
 
-	/** 获得做对的比例数 */
-	public double getScore() {
-		if (score == 0)
-			this.calcScore();
-		return score;
-	}
+    /** 获得做对的比例数 */
+    public double getScore() {
+        if (score == 0)
+            this.calcScore();
+        return score;
+    }
 
-	/** 做对了1个 */
-	public void addCouTue() {
-		this.couTue++;
-		this.addCouTeam();
-	}
+    /** 做对了1个 */
+    public void addCouTue() {
+        this.couTue++;
+        this.addCouTeam();
+    }
 
-	/** 做错了1个 */
-	public void addCouFal() {
-		this.couFal++;
-		this.addCouTeam();
-	}
+    /** 做错了1个 */
+    public void addCouFal() {
+        this.couFal++;
+        this.addCouTeam();
+    }
 
-	/* 做了1个 */
-	private void addCouTeam() {
-		this.couTeam++;
-	}
+    /* 做了1个 */
+    private void addCouTeam() {
+        this.couTeam++;
+    }
 
-	/* 计算得分 */
-	private void calcScore() {
-		this.score = couTue * 1.0 / couTeam;
-		this.score = Double.parseDouble(String.format("%.3f", score));
-	}
+    /* 计算得分 */
+    private void calcScore() {
+        this.score = couTue * 1.0 / couTeam;
+        this.score = Double.parseDouble(String.format("%.3f", score));
+    }
 
-	/** 输出结果 */
-	public void printResult() {
-		System.out.println("true :" + getCouTue());
-		System.out.println("false:" + getCouFal());
-		System.out.println("score:" + getScore());
-		System.out.println("----------------------------------------");
-	}
+    /** 输出结果 */
+    public void printResult() {
+        System.out.println("true : " + getCouTue());
+        System.out.println("false: " + getCouFal());
+        System.out.println("score: " + (getScore() * 100) + "%");
+        System.out.println("----------------------------------------");
+    }
 
 }
