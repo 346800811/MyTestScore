4a8a86a308073a11a74156dab30e185ba8f2f7a4
De <346800811@qq.com>
2018-02-24 17:12:21 +0800

    commit

---
+++ MyTestSc/src/com/topic/passPmpPdf/PassPmpPdf01_0.java
@@ -0,0 +1,23 @@
+package com.topic.passPmpPdf;
+
+import com.base.BaseTest;
+
+/**
+ * 成功通过PMP-PDF（P0）
+ * 
+ * @author JianDe
+ */
+public class PassPmpPdf01_0 extends BaseTest {
+    public PassPmpPdf01_0(String result) {
+        super("");
+    }
+
+    public static void main(String[] args) {
+        BaseTest pm1 = new PassPmpPdf01_0(null);
+        pm1.test(""); // 2018-01-01
+    }
+
+}
+/*
+ * 
+ */
---
+++ MyTestSc/src/com/topic/passPmpPdf/PassPmpPdf13_1.java
@@ -0,0 +1,23 @@
+package com.topic.passPmpPdf;
+
+import com.base.BaseTest;
+
+/**
+ * 成功通过PMP-PDF（P0）
+ * 
+ * @author JianDe
+ */
+public class PassPmpPdf13_1 extends BaseTest {
+    public PassPmpPdf13_1(String result) {
+        super("");
+    }
+
+    public static void main(String[] args) {
+        BaseTest pm1 = new PassPmpPdf13_1(null);
+        pm1.test(""); // 2018-01-01
+    }
+
+}
+/*
+ * 
+ */
