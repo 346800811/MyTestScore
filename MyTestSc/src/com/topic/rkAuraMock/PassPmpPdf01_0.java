0a4a1e10638d5cdfa98e4f74b4ab63ee93a9e885
Administrator <Administrator@749S7K6CZFKAHCH>
2018-03-27 12:44:20 +0800

    commit

---
+++ MyTestSc/src/com/topic/rkAuraMock/PassPmpPdf01_0.java
@@ -0,0 +1,23 @@
+package com.topic.rkAuraMock;
+
+import com.base.BaseTest;
+
+/**
+ * 课前自测75题
+ * 
+ * @author JianDe
+ */
+public class PassPmpPdf01_0 extends BaseTest {
+    public PassPmpPdf01_0() {
+        super("ABCCDCBCBAACDCCCDADACABBADCDCDCCDCADCAACDDDBACDCAAACBBBBDCCBACBDCBBCBBCCABB");
+    }
+
+    public static void main(String[] args) {
+        BaseTest pm1 = new PassPmpPdf01_0();
+        pm1.test("DDDCD ABDCA BADCC BDADA ACBAA CCCCC DCDCD BCDCD ACBCB DCCCB ACDBD DDABA DBBBC CCCBB AACCC"); // 2018-03-22
+    }
+
+}
+/*
+ * 
+ */
